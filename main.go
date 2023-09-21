2e01ddc994a96f351c6b3b2b1b14e264add407b2
jaspreet39 <jassingh.0244@gmail.com>
2023-09-21 19:44:33 -0400

    Add swapping fuction in main.go file

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 func main() {
 
@@ -10,4 +13,35 @@ func main() {
 	sum := a + b
 
 	fmt.Println(sum)
+	calculator()
+}
+
+func calculator() {
+	fmt.Println("Enter Your First Number: ")
+
+	// var then variable name then variable type
+	var first int
+	fmt.Scanln(&first)
+
+	fmt.Println("Enter Your Second Number: ")
+
+	// var then variable name then variable type
+	var second int
+	fmt.Scanln(&second)
+
+	fmt.Println("Press 1 for add, 2 for multiply and 3 for substract")
+
+	switch os := runtime.GOOS; os {
+	case "1":
+		fmt.Println(first + second)
+	case "2":
+		fmt.Println(first * second)
+	case "3":
+		fmt.Println(first - second)
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
+	}
+
 }
